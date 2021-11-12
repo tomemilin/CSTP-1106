b520a7f909c90273e10e4aacc1ba6d51bb2eb2ac
tomemilin <milin.tome@gmail.com>
2021-11-11 22:04:37 -0800

    labweek10

---
+++ Labweek10/code.js
@@ -0,0 +1,12 @@
+add = function() {
+    x = parseInt(jQuery('#op1').html()); // html() is a getter
+    y = parseInt(jQuery('#op2').html());
+    // alert(x + y) //now, adition not concatenation anymore
+    z = x + y // processing
+    jQuery('#result').html(z); // html(inout) is a setter
+}
+setup = function() {
+    jQuery('#trigger_the_add').click(add)
+}
+
+jQuery(document).ready(setup)
