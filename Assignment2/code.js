fb2fc3067881f7fcabcd9d27d60db5aa56df1984
tomemilin <milin.tome@gmail.com>
2021-11-24 19:33:21 -0800

    updated the hash

--- Assignment2/code.js
+++ Assignment2/code.js
@@ -34,12 +34,11 @@ divide = function() {
 power = function(){
   x = parseInt(jQuery('#OP1').val());
   y = parseInt(jQuery('#OP2').val());
-  exp = 1;
+  let exp = 1;
     for (let i = 1; i <= y; i++)
     {
       exp = exp*x;
     }
-    z = exp;
   valu = '<span>' + x  + ' to the power ' + y +  ' = ' + z + '<button id ="delete">delete</button>' + '<br>' + '</span>';
   jQuery('#result').append(valu);
 }
@@ -62,7 +61,7 @@ setup = function() {
     jQuery('#trigger_the_subtract').click(subtract)
     jQuery('#trigger_the_hide').click(hide1)
     jQuery('#trigger_the_show').click(show1)
-    jQuery("trigger_the_power").click(power)
+    jQuery("#trigger_the_power").click(power)
     jQuery('body').on('click', '#delete', delete1)
 }
 
