fdb0fa3b90dbf9f9f05be1e6284a5d64ddfea987
tomemilin <milin.tome@gmail.com>
2021-12-12 18:32:21 -0800

    Update Movies.js

--- IMDB/Movies.js
+++ IMDB/Movies.js
@@ -1,11 +1,12 @@
-function procces_(){
-    console.log('#div1')
+function procces_(data1){
+    // console.log(data1)
+    
 }
 function sites_api(){
   $.ajax(
 
     {
-      url:'https://imdb-api.com/en/API/Top250Movies/k_12345678',
+      url:'https://imdb-api.com/en/API/Top250Movies/k_4z92p35f',
       type:'GET',
       success: procces_
     }
@@ -14,7 +15,8 @@ function sites_api(){
 }
 
 function setup(){
-   jQuery().load('sties_api');
+   // jQuery().load('sties_api');
+   sites_api()
 }
 
 jQuery(document).ready(setup)
